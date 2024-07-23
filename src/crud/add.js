965b0543f71a02fba02d930de87c495b1d2589f6
kamilo9809 <camilorivera09@outlook.com>
2024-07-22 20:13:25 -0500

    first commit

---
+++ src/crud/add.js
@@ -0,0 +1,14 @@
+import { db } from "../main"
+
+const data = document.querySelector('#data')
+
+export const addTask=()=>{
+    if (!data.value) {
+        alert('ingresa una tarea')
+    }else{
+        db.push({task:data.value, edit:'editClick', delete:'deleteClick'})
+        localStorage.setItem('db', JSON.stringify(db))      
+    }
+    data.value = ''
+    location.reload()
+}
---
+++ src/crud/getAll.js
@@ -0,0 +1,18 @@
+import { db } from "../main"
+
+
+export function getData() {
+  document.querySelector('#app').innerHTML=`
+  <div>
+   ${db.map((array)=>`
+   <div class="flex justify-between gap-10">
+     <h2>${array.task}</h2>
+     <div class="flex">
+      <button class="${array.edit}" >✏️</button>
+      <button class="${array.delete}" >❌</button>
+     </div>
+   </div>
+   `).join('')}
+  </div>
+  `
+}
